ebf83b8d91865300d9f9e05103dcb7e125a20d00
PHAT2417 <dragblacker214@gmail.com>
2023-07-01 19:36:23 +0700

    a

--- file-js/insert.js
+++ file-js/insert.js
@@ -14,12 +14,22 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const database = firebase.database();
-
-
-function dragdrop(event){
-    var filename = URL.createObjectURL(event.target)
-}
+firebase.initializeApp(firebaseConfig);
+   console.log(firebase);
+   function uploadImage() {
+      const ref = firebase.storage().ref();
+      const file = document.querySelector("#photo").files[0];
+      const name = +new Date() + "-" + file.name;
+      const metadata = {
+         contentType: file.type
+      };
+      const task = ref.child(name).put(file, metadata);task
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+      console.log(url);
+      alert('image uploaded successfully');
+      document.querySelector("#image").src = url;
+   })
+   .catch(console.error);
+   }
+   const errorMsgElement = document.querySelector('span#errorMsg');
